6a195e4e88a08700c326e254ec9308879cdb2582
bboti199 <b.boti199@yahoo.com>
2020-07-02 21:42:54 +0300

    Initial commit

---
+++ TrickingLibrary.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrickingLibrary.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World!";
+        }
+    }
+}
